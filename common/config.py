2fb5d80dcd92716dedd013e9e352a246d7f6de04
手瓜一十雪 <nanaeonn@outlook.com>
2025-01-03 13:23:05 +0800

    fix

---
+++ common/config.py
@@ -0,0 +1,8 @@
+config = {
+     "cookie" : "",
+     "repo" : "NapNeko/NapCatQQ",
+     "promote": "注意请你使用中文回答问题, src/onebot/action/router.ts 里面存在napcat onebot接口端点,借助这个可以找到 Action类 进而找到 SchemaData 定义和原理,请你不要直接给出raw，而是从onebot协议和通讯格式( https://github.com/botuniverse/onebot-11/blob/master/communication/http.md  https://github.com/botuniverse/onebot-11/blob/master/communication/ws.md )调用方面解答napcat问题,给出具体JSON演示和接口定义,问题:" ,
+     "proxy": "http://127.0.0.1:5967",
+     "self_id": 10001,
+     "websocket_onebot":"ws://127.0.0.1:3001/"
+}
